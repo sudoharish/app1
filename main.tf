446b517ae73d797b27f23b378fe034359f07aecc
Harish Ahmad <Harishahmad8010@gmail.com>
2025-07-13 15:55:12 +0000

    updated

--- main.tf
+++ main.tf
@@ -13,3 +13,12 @@ provider "aws" {
   shared_config_files       = ["creds/aws/config"]
   shared_credentials_files  = ["creds/aws/credentials"]
 }
+<<<<<<< HEAD:main.tf
+=======
+
+backend "s3" {
+  bucket = "my-terraform-state"
+  key    = "project/app1.tfstate"
+  region = "ap-south-1"
+}
+>>>>>>> 906ab89 (updated):terraform/main.tf
